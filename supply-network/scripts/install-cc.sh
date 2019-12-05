e9bebd8099418cc76471727d8ffed36b6f4db0d3
Iván <ivan.alberquilla@gmail.com>
2019-12-05 16:50:37 +0100

    remove duplicate sh

--- supply-network/scripts/install-cc.sh
+++ supply-network/scripts/install-cc.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "Installing chaincode for producer..."
-docker exec -it cli ./scripts/install-cc/install-producer.sh "1.0"
+docker exec -it cli ./scripts/install-cc/install-peer.sh peer0 producer ProducerMSP 7051 1.0
 echo "Installing chaincode for manufacturer..."
-docker exec -it cli ./scripts/install-cc/install-manufacturer.sh "1.0"
+docker exec -it cli ./scripts/install-cc/install-peer.sh peer0 manufacturer ManufacturerMSP 9051 1.0
 echo "Installing chaincode for deliverer..."
-docker exec -it cli ./scripts/install-cc/install-deliverer.sh "1.0" 
+docker exec -it cli ./scripts/install-cc/install-peer.sh peer0 deliverer DelivererMSP 10051 1.0
 echo "Installing chaincode for retailer..."
-docker exec -it cli ./scripts/install-cc/install-retailer.sh "1.0"
+docker exec -it cli ./scripts/install-cc/install-peer.sh peer0 retailer RetailerMSP 11051 1.0
 echo "Instanciating the chaincode..."
-docker exec -it cli ./scripts/install-cc/instanciate.sh "1.0"
+docker exec -it cli ./scripts/install-cc/instanciate.sh 
---
+++ supply-network/scripts/install-cc/install-peer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+PEER=$1
+ORG=$2
+MSP=$3
+PORT=$4
+VERSION=$5
+
+ORDERER_CA=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+CORE_PEER_LOCALMSPID=$MSP
+CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/$ORG.example.com/peers/$PEER.$ORG.example.com/tls/ca.crt
+CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/$ORG.example.com/users/Admin@$ORG.example.com/msp
+CORE_PEER_ADDRESS=$PEER.$ORG.example.com:$PORT
+CHANNEL_NAME=mychannel
+CORE_PEER_TLS_ENABLED=true
+peer chaincode install -l node -n supcc -v $VERSION -p /opt/gopath/src/github.com/chaincode >&log.txt
+cat log.txt
